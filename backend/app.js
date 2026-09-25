caec68a7ccef21ebe91c223b86a648a3bd792ea1
Vkay23 <57458523+Vkay23@users.noreply.github.com>
2026-09-25 02:48:04 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path'; // Added for path resolution
 import product from './routes/productRoutes.js';
 import user from './routes/userRoutes.js';
 import order from './routes/orderRoutes.js';
@@ -6,7 +7,7 @@ import payment from './routes/paymentRoutes.js';
 import errorHandleMiddleware  from './middleware/error.js';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 const app=express();
 
 // Middleware
@@ -15,11 +16,23 @@ app.use(cookieParser())
 app.use(fileUpload())
 app.use(express.urlencoded({ extended: true }));
 
-// Route
+// Routes
 app.use("/api/v1",product)
 app.use("/api/v1",user)
 app.use("/api/v1",order)
 app.use("/api/v1",payment)
+
+// --- SERVE FRONTEND STATIC PRODUCTION FILES ---
+const __dirname = path.resolve();
+
+// Serve the assets folder from your frontend build directory
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+// Redirect any non-API get requests to React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+// ----------------------------------------------
 
 app.use(errorHandleMiddleware)
 dotenv.config({path:'backend/config/config.env'})
